5cbbefb1bf2b9dccfc3db759a1c59e67e2038710
GitHub Action <action@github.com>
2026-07-17 17:18:30 +0000

    chore: update fullfetch to v3.1.0

--- Formula/fullfetch.rb
+++ Formula/fullfetch.rb
@@ -3,28 +3,28 @@ class Fullfetch < Formula
   homepage "https://github.com/Buct0r/fullfetch"
   license "MIT"
   
-  version "v3.0.0"
+  version "v3.1.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/Buct0r/fullfetch/releases/download/v3.0.0/fullfetch_Darwin_arm64.tar.gz"
-      sha256 "781ab13cd4dac5c7f0a7d7a8b98ff60136c7becc18fc447447e454d366dda086"
+      url "https://github.com/Buct0r/fullfetch/releases/download/v3.1.0/fullfetch_Darwin_arm64.tar.gz"
+      sha256 "657fc7bfaf4f5590b559bcb90d46858ec59ffbad7453b9a87f3181b781dca152"
     end
 
     on_intel do
-      url "https://github.com/Buct0r/fullfetch/releases/download/v3.0.0/fullfetch_Darwin_x86_64.tar.gz"
-      sha256 "4890d53362afe3efb0a33678289cf51f2c5f971d177d24ca7b81a080673138f1"
+      url "https://github.com/Buct0r/fullfetch/releases/download/v3.1.0/fullfetch_Darwin_x86_64.tar.gz"
+      sha256 "708a2ef99e928ebc170fe2adfcb27c06855b1f06a9eea86e31c1fed415453b2c"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/Buct0r/fullfetch/releases/download/v3.0.0/fullfetch_Linux_arm64.tar.gz"
-      sha256 "3a53496e7bc796b803c2ffa084b17119b715861165b5bf62d154fb0c9c636278"
+      url "https://github.com/Buct0r/fullfetch/releases/download/v3.1.0/fullfetch_Linux_arm64.tar.gz"
+      sha256 "15ed2b1f922f74c3732664a6d70c8ede5a79e33ed482d8230ce264dc5aec0fbe"
     end
     on_intel do
-      url "https://github.com/Buct0r/fullfetch/releases/download/v3.0.0/fullfetch_Linux_x86_64.tar.gz"
-      sha256 "d75995106ab7221310099c7ee79c98c444cb3c823177dc6f853968a86e46aa09"
+      url "https://github.com/Buct0r/fullfetch/releases/download/v3.1.0/fullfetch_Linux_x86_64.tar.gz"
+      sha256 "b2cc83c14d83d05d23f9579877a7a7f22e625c8e9cb7cd060219a37c2cd862b4"
     end
   end
 
